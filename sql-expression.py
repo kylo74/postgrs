16a6ca8e4dcee30898c546426737a9666a7c8e35
kylo74 <kyloc74@gmail.com>
2022-04-12 13:13:01 +0000

    Query table using sqlalchemy

---
+++ sql-expression.py
@@ -0,0 +1,53 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+# create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+# create variable for "Album" table
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+# create variable for "Table" table
+track_table = Table(
+    "Track", meta,
+    Column("Track", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+)
+
+# making the connection
+with db.connect() as connection:
+
+    # Query 1 - select all records from the artist table
+    # select_query = artist_table.select()
+
+    # Query 2 - select onlu the "Name" column form the "Artist" Table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Query 3 - select only 'Queen' from the "Artist" table
+    select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
