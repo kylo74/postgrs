a91176f6067b8707a93e6bba1bb728a7fb599aaf
kylo74 <kyloc74@gmail.com>
2022-04-15 11:35:29 +0000

    Finish adding programmers

--- sql-crud.py
+++ sql-crud.py
@@ -20,6 +20,7 @@ class Programmer(base):
     nationality = Column(String)
     famous_for = Column(String)
 
+
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
@@ -30,80 +31,75 @@ session = Session()
 base.metadata.create_all(db)
 
 
-# creating the database using declarative_base subclass
-base.metadata.create_all(db)
-
-
-# creating records on our Programmer table
+# creating records on our Progammer table
 ada_lovelace = Programmer(
-    first_name = "Ada",
-    last_name = "Lovelace",
-    gender = "F",
-    nationality = "British",
-    famous_for = "First Programmer"
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer"
 )
 
 alan_turing = Programmer(
-    first_name = "Alan",
-    last_name = "Turing",
-    gender = "M",
-    nationality = "British",
-    famous_for = "Modern Computing"
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
 )
 
 grace_hopper = Programmer(
-    first_name = "Grace",
-    last_name = "Hopper",
-    gender = "F",
-    nationality = "American",
-    famous_for = "COBOL language"
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
 )
 
-margret_hamilton = Programmer(
-    first_name = "Margret",
-    last_name = "Hamilton",
-    gender = "F",
-    nationality = "American",
-    famous_for = "Apollo 11"
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
 )
 
 bill_gates = Programmer(
-    first_name = "Bill",
-    last_name = "Gates",
-    gender = "M",
-    nationality = "American",
-    famous_for = "Microsoft"
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
 )
 
 tim_berners_lee = Programmer(
-    first_name = "Tim",
-    last_name = "Berners-Lee",
-    gender = "M",
-    nationality = "British",
-    famous_for = "World Wide Web"
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
 )
-
 
 # add each instance of our programmers to our session
 # session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
 
+# commit our session to the database
+# session.commit()
 
-# commit  out session to the database
-session.commit()
 
 # query the database to find all Programmers
-progammers = session.query(Programmer)
-for programmer in progammers:
+programmers = session.query(Programmer)
+for programmer in programmers:
     print(
         programmer.id,
         programmer.first_name + " " + programmer.last_name,
         programmer.gender,
         programmer.nationality,
         programmer.famous_for,
-        sep= " | "
+        sep=" | "
     )
